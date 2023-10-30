9f9ebb0d4625b8570017cd5d06128897db517b65
Baktybek <Baktybek.Zhumabekov@timelysoft.net>
2023-10-30 16:38:28 +0600

    added method for find frequent letter

--- AlgoritmTasks/AlgoritmTasks/AnyAlgoritms.cs
+++ AlgoritmTasks/AlgoritmTasks/AnyAlgoritms.cs
@@ -8,5 +8,31 @@ namespace AlgoritmTasks
 {
     public static class AnyAlgoritms
     {
+        /// <summary>
+        /// Find frequent letter, and show count
+        /// </summary>
+        public static void FindFrequentLetter()
+        {
+            var str = Console.ReadLine();
+            var dict = new Dictionary<char, int>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!dict.ContainsKey(str[i]))
+                {
+                    dict.Add(str[i],1);
+                }
+                else
+                {
+                    dict[str[i]] = dict[str[i]] + 1;
+                }
+            }
+
+            int maxValue = dict.Max(x => x.Value);
+            var result = dict.Where(x => x.Value == maxValue);
+            foreach (var item in result)
+            {
+                Console.WriteLine($"Letter- {item.Key}: Count-{item.Value}");
+            }
+        }
     }
 }
