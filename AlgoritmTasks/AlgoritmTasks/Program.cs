f8269e433594824e96f613955d11078dd2c576cf
Baktybek <Baktybek.Zhumabekov@timelysoft.net>
2023-10-30 14:33:43 +0600

    Init commit

---
+++ AlgoritmTasks/AlgoritmTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoritmTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
